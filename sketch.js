f02878db3849bd2eff9d16ecc2dd5cb87c28737b
ericinthehaus <31998942+ericinthehaus@users.noreply.github.com>
2017-10-14 17:22:03 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var xpos= 50;
+var ypos=  0;
+var xsize = 20;
+
+var ground=0;
+
+var num;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+        background(255);
+        stroke (0,0,255);
+        fill (0,0,255);
+        rect (10, 10, 75,35);
+        fill(255,255,0);
+        textSize (30);
+        text ("FIFA", 17,40);
+    
+        fill ()
+    }
+    
